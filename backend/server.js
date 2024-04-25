c51c90ba1225885711811837de780754459c25a7
Ravi Bhaskar <ravi.bhaskar@tecvololabs.com>
2024-04-26 00:31:20 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 const userRouter = require("./routes/userRoute")
